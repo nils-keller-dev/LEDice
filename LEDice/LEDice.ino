1321b76079984eb180688e99d77e11197727eba7
tsomic <keller.nils99@gmail.com>
2024-06-15 01:32:16 +0200

    ✨ add D4, D8, D12 and D20

--- LEDice/LEDice.ino
+++ LEDice/LEDice.ino
@@ -1,25 +1,40 @@
 #include <LedControl.h>
 #include "button.h"
 
-#define CS_PIN  PB0
+#define CS_PIN PB0
 #define CLK_PIN PB1
 #define DIN_PIN PB2
 #define MAX_SEG 1
 #define ROLL_PIN PB4
-#define NUMBER_DICE_PIN A3
+#define MODE_PIN A3
 #define RND_PIN A0
 #define ROLL_TIME 300
 
-const uint8_t dice[6][8] = {
+const uint8_t decimal[10][8] = {
+  { B010, B101, B101, B101, B101, B101, B101, B010 },
+  { B001, B011, B101, B001, B001, B001, B001, B001 },
+  { B010, B101, B101, B001, B010, B100, B100, B111 },
+  { B110, B001, B001, B010, B001, B001, B001, B110 },
+  { B100, B100, B101, B111, B001, B001, B001, B001 },
+  { B111, B100, B100, B111, B001, B001, B101, B010 },
+  { B010, B101, B100, B110, B101, B101, B101, B010 },
+  { B111, B001, B001, B010, B010, B100, B100, B100 },
+  { B010, B101, B101, B010, B101, B101, B101, B010 },
+  { B010, B101, B101, B011, B001, B001, B101, B010 },
+};
+
+const uint8_t dot[8][8] = {
   { B00000000, B00000000, B00000000, B00011000, B00011000, B00000000, B00000000, B00000000 },
   { B00000011, B00000011, B00000000, B00000000, B00000000, B00000000, B11000000, B11000000 },
   { B00000011, B00000011, B00000000, B00011000, B00011000, B00000000, B11000000, B11000000 },
   { B11000011, B11000011, B00000000, B00000000, B00000000, B00000000, B11000011, B11000011 },
   { B11000011, B11000011, B00000000, B00011000, B00011000, B00000000, B11000011, B11000011 },
-  { B11000011, B11000011, B00000000, B11000011, B11000011, B00000000, B11000011, B11000011 }
+  { B11000011, B11000011, B00000000, B11000011, B11000011, B00000000, B11000011, B11000011 },
+  { B11000011, B11000011, B00000000, B11011011, B11011011, B00000000, B11000011, B11000011 },
+  { B11011011, B11011011, B00000000, B11000011, B11000011, B00000000, B11011011, B11011011 },
 };
 
-const uint8_t smallDice[6][3] = {
+const uint8_t smallDot[6][3] = {
   { B000, B010, B000 },
   { B100, B000, B001 },
   { B100, B010, B001 },
@@ -28,13 +43,15 @@ const uint8_t smallDice[6][3] = {
   { B101, B101, B101 },
 };
 
+const uint8_t diceSidesCount[9] = { 6, 6, 6, 6, 4, 8, 10, 12, 20 };
+
 LedControl* matrix;
 bool diceRolling = false;
 uint32_t buttonPressTime = 0;
-uint8_t numberDice;
+uint8_t selectedDice = 0;
 
-Button btnRollDice(ROLL_PIN);
-Button btnNumberDice(NUMBER_DICE_PIN);
+Button btnRoll(ROLL_PIN);
+Button btnMode(MODE_PIN);
 
 void rollDice(bool setup = false) {
   if (!setup) {
@@ -42,10 +59,10 @@ void rollDice(bool setup = false) {
     delay(ROLL_TIME);
   }
 
-  if (numberDice == 0) {
+  if (selectedDice == 0 || diceSidesCount[selectedDice] != 6) {
     rollSingleDice();
   } else {
-    rollMultipleDice();
+    rollMultipleDice(selectedDice + 1);
   }
 }
 
@@ -55,8 +72,8 @@ void setup() {
 
   randomSeed(analogRead(RND_PIN));
 
-  btnRollDice.begin();
-  btnNumberDice.begin();
+  btnRoll.begin();
+  btnMode.begin();
 
   matrix->shutdown(0, false);
 
@@ -66,7 +83,7 @@ void setup() {
     intensity += 5;
   }
 
-  if (!digitalRead(NUMBER_DICE_PIN)) {
+  if (!digitalRead(MODE_PIN)) {
     intensity += 10;
   }
 
@@ -75,49 +92,79 @@ void setup() {
   rollDice(true);
 }
 
-void changeNumberDice() {
-  numberDice = (numberDice + 1) % 4;
+void changeMode() {
+  selectedDice = (selectedDice + 1) % 9;
 }
 
-void rollSingleDice() {
-  uint8_t number = random(6);
+void rollDecimal(uint8_t number) {
+  uint8_t digits[2] = {};
+  int index = 1;
 
-  for (uint8_t i = 0; i <= 7; i++) {
-    matrix->setRow(0, i, dice[number][i]);
+  while (number > 0) {
+    int digit = number % 10;
+    digits[index] = digit;
+    number /= 10;
+    index--;
+  }
+
+  if (digits[0] != 0) {
+    for (uint8_t i = 0; i < 8; i++) {
+      matrix->setRow(0, i, decimal[digits[0]][i] << 5 | decimal[digits[1]][i]);
+    }
+  } else {
+    for (uint8_t i = 0; i < 8; i++) {
+      matrix->setRow(0, i, decimal[digits[1]][i]);
+    }
   }
 }
 
-void rollMultipleDice() {
-  uint8_t numbers[numberDice + 1];
+void rollDot(uint8_t number) {
+  for (uint8_t i = 0; i <= 7; i++) {
+    matrix->setRow(0, i, dot[number - 1][i]);
+  }
+}
 
-  for (uint8_t i = 0; i <= numberDice; i++) {
+void rollSingleDice() {
+  uint8_t number = random(1, diceSidesCount[selectedDice] + 1);
+
+  if (selectedDice < 6) {
+    rollDot(number);
+  } else {
+    rollDecimal(number);
+  }
+}
+
+void rollMultipleDice(uint8_t count) {
+  uint8_t numbers[count];
+
+  for (uint8_t i = 0; i <= count; i++) {
     numbers[i] = random(6);
   }
 
   for (uint8_t i = 0; i <= 2; i++) {
-    matrix->setRow(0, i, smallDice[numbers[0]][i] << 5 | smallDice[numbers[1]][i]);
+    matrix->setRow(0, i, smallDot[numbers[0]][i] | smallDot[numbers[1]][i] << 5);
   }
 
-  if (numberDice < 2) {
+  if (count <= 2) {
     return;
   }
 
   for (uint8_t i = 0; i <= 2; i++) {
-    uint8_t rowValue = smallDice[numbers[2]][i] << 5;
-    if (numberDice == 3) {
-      rowValue |= smallDice[numbers[3]][i];
+    uint8_t rowValue = smallDot[numbers[2]][i] << 5;
+    if (count == 4) {
+      rowValue |= smallDot[numbers[3]][i];
     }
     matrix->setRow(0, i + 5, rowValue);
   }
 }
 
 void loop() {
-  if (btnRollDice.isPressed()) {
+  if (btnRoll.isPressed()) {
     rollDice();
   }
 
-  if (btnNumberDice.isPressed()) {
-    changeNumberDice();
+  if (btnMode.isPressed()) {
+    changeMode();
     rollDice();
   }
 }
